ff474a6b1ad141b685587867bd1758563de7cf76
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-15 12:48:10 +0000

    Edited 'main.py'

--- TotalVehicles/main.py
+++ TotalVehicles/main.py
@@ -32,7 +32,7 @@ def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.Dat
             if row.get(vehicle_type, 0) > 0:
                 vehicle_counts[vehicle_type] += 1
                 row_vehicles += 1
-            row["vehicles"] = row_vehicles
+            row["row_vehicles"] = row_vehicles
             print(row)
 
     total_vehicles = 0

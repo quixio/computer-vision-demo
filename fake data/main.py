c02cb0fd26c68849c5de526e59e10c6f25cb9bf7
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-15 13:42:09 +0000

    Edited 'main.py'

--- fake data/main.py
+++ fake data/main.py
@@ -1,12 +1,37 @@
 import quixstreams as qx
+import time
+import datetime
+import math
 import os
+import random
 
-# Quix injects credentials automatically to the client.
+
+# Quix injects credentials automatically to the client. 
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
 
-# Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
+# Open the output topic where to write data out
+topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
 
-# for more samples, please see samples or docs
+# Set stream ID or leave parameters empty to get stream ID generated.
+stream = topic_producer.create_stream()
+stream.properties.name = "CAM001"
+
+# Add metadata about time series data you are about to send. 
+print("Sending values for a while....")
+
+for index in range(0, 3000):
+    stream.timeseries \
+        .buffer \
+        .add_timestamp(datetime.datetime.utcnow()) \
+        .add_value("car",  random.randint(1, 10)) \
+        .add_value("bus",  random.randint(1, 10)) \
+        .add_value("truck",  random.randint(1, 10)) \
+        .add_value("lat",  random.randint(1, 10)) \
+        .add_value("lon",  random.randint(1, 10)) \
+        .add_value("delta",  random.randint(1, 10)) \
+        .publish()
+    time.sleep(1)
+
+print("Closing stream")
+stream.close()

c61952fe8140b41b01e215c7af8e7c6bc73d5121
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-15 12:24:36 +0000

    Edited 'main.py'

--- TotalVehicles/main.py
+++ TotalVehicles/main.py
@@ -1,12 +1,39 @@
 import quixstreams as qx
 import os
+import pandas as pd
+import datetime
 
-# Quix injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
+
 client = qx.QuixStreamingClient()
 
-# Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
+topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
+topic_producer = client.get_topic_producer(os.environ["output"])
 
-# for more samples, please see samples or docs
+pd.set_option('display.max_columns', None)
+
+
+def ts_to_date(ts):
+    sec = ts / 1_000_000_000
+    dt = datetime.datetime.utcfromtimestamp(sec)
+    #print(dt)
+    return dt
+
+
+def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    print(stream_consumer.stream_id)
+    #if stream_consumer.stream_id == "JamCams_00001.01404":
+    #    print("HERE!")
+        
+
+
+def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+
+
+# subscribe to new streams being received
+topic_consumer.on_stream_received = on_stream_received_handler
+
+print("Listening to streams. Press CTRL-C to exit.")
+
+# Handle termination signals and provide a graceful exit
+qx.App.run()

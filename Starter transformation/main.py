69091b568adb4d2c0f4e25aa7c374f1d3ee770f0
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 07:46:46 +0000

    Edited 'main.py'

--- Starter transformation/main.py
+++ Starter transformation/main.py
@@ -19,8 +19,8 @@ cams = {
 
 start_of_window = None
 end_of_window = None
-window_length_days = 0
-window_length_mins = 120
+window_length_days = 1
+window_length_mins = 0
 window_length_secs = 0
 
 def update_window():

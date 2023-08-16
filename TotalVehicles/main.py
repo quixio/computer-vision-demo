e5d0a295510e80abc0698150e02e241f31b73664
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 06:46:39 +0000

    Edited 'main.py'

--- TotalVehicles/main.py
+++ TotalVehicles/main.py
@@ -21,15 +21,10 @@ def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.Dat
     # Calculate the total vehicle count based on existing columns
     total_vehicle_count = df.apply(lambda row: sum(row.get(column, 0) for column in vehicle_columns), axis=1)
 
-    df_out = pd.DataFrame()
-    # df_out["vehicles"] = total_vehicle_count
-    # df_out["lat"] = df["lat"]
-    # df_out["lon"] = df["lon"]
-    # df_out["delta"] = df["delta"]
+    df["vehicles"] = total_vehicle_count
 
-    print(df_out)
     stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-    stream_producer.timeseries.buffer.publish(df_out)
+    stream_producer.timeseries.buffer.publish(df)
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler

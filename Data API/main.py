1ccb4b7ef2b28c2dc19b93bf0f33c04af5de3fa4
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 09:57:49 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -1,6 +1,26 @@
+import quixstreams as qx
 from flask import Flask
+import os
 
+# init the flas app
 app = Flask(__name__)
+
+
+# Quix injects credentials automatically to the client.
+# Alternatively, you can always pass an SDK token manually as an argument.
+client = qx.QuixStreamingClient()
+
+print("Opening input topic")
+consumer_topic = client.get_topic_consumer(os.environ["input"])
+
+def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    print(df)
+
+
+def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+
+
 
 @app.route("/")
 def index():

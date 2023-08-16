6c42d70bf8eb48e3a58a89b59460e2af1744dfca
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 09:26:09 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=80)

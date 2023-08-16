4094434f52edaf47f4e0a1770fd0aa43cf641265
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 10:26:11 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -6,9 +6,9 @@ import os
 pd.set_option('display.max_columns', None)
 
 max_vehicles = {}
+detected_objects = {}
 
-# init the flas app
-app = Flask(__name__)
+
 
 
 # Quix injects credentials automatically to the client.
@@ -16,10 +16,11 @@ app = Flask(__name__)
 client = qx.QuixStreamingClient()
 
 print("Opening input topic")
-consumer_topic = client.get_topic_consumer(os.environ["input"])
+max_veh_topic = client.get_topic_consumer(os.environ["input"])
+object_topic = client.get_topic_consumer(os.environ["objects"])
 
 
-def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
+def on_max_veh_stream_received_handler(stream_consumer: qx.StreamConsumer):
     global max_vehicles
 
     def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
@@ -29,22 +30,48 @@ def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
 
 
+def on_object_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    global detected_objects
+
+    def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+        #print(f'stream:{stream_consumer.stream_id}, data={df["max_vehicles"][0]}')
+        #max_vehicles[stream_consumer.stream_id] = df["max_vehicles"][0]
+        pass
+
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+
+
+# init the flas app
+app = Flask(__name__)
+
+# create the default route
 @app.route("/")
 def index():
     return "hello"
 
+# create the max_vehicles route
 @app.route("/max_vehicles")
 def maximum_vehicles():
     return max_vehicles
+
+# create the detected objects route
+@app.route("/detected_objects")
+def objects():
+    return detected_objects
 
 if __name__ == "__main__":
     print("main..")
     from waitress import serve
 
     # hook up the stream received handler
-    consumer_topic.on_stream_received = on_stream_received_handler
+    max_veh_topic.on_stream_received = on_max_veh_stream_received_handler
     # subscribe to data arriving into the topic
-    consumer_topic.subscribe()
+    max_veh_topic.subscribe()
+
+    # hook up the stream received handler
+    object_topic.on_stream_received = on_object_stream_received_handler
+    # subscribe to data arriving into the topic
+    object_topic.subscribe()
 
     # you can use app.run for dev, but its not secure, stable or particularly efficient
     # app.run(debug=True, host="0.0.0.0", port=80)

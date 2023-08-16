c33978b26d87125c90ffbee4b5825996df1d8df4
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 09:25:13 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -2,7 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)

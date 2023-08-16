bb0f1ca5c320c1ce6593d09b2b0769e5c49e56d3
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-16 09:18:29 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -1,12 +1,8 @@
-import quixstreams as qx
-import os
+from flask import Flask
 
-# Quix injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
-client = qx.QuixStreamingClient()
+app = Flask(__name__)
 
-# Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
 
-# for more samples, please see samples or docs
+@app.route('/')
+def hello():
+    return 'Hello, World!'

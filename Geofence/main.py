1248d5e778574eb38f6915e3312b77f72921d43e
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-23 11:50:33 +0000

    Edited 'main.py'

--- Geofence/main.py
+++ Geofence/main.py
@@ -1,12 +1,31 @@
 import quixstreams as qx
 import os
+import pandas as pd
+import json
 
-# Quix injects credentials automatically to the client.
-# Alternatively, you can always pass an SDK token manually as an argument.
+
 client = qx.QuixStreamingClient()
 
-# Use Input / Output topics to stream data in or out of your service
-consumer_topic = client.get_topic_consumer(os.environ["input"])
-producer_topic = client.get_topic_producer(os.environ["output"])
+topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "geofence", 
+                                            auto_offset_reset = qx.AutoOffsetReset.Latest)
+topic_producer = client.get_topic_producer(os.environ["output"])
 
-# for more samples, please see samples or docs
+pd.set_option('display.max_columns', None)
+
+def on_event_data_handler(self, stream_consumer: qx.StreamConsumer, data: qx.EventData):
+    
+    camera = json.loads(data.value)
+    lon = float(camera["lon"])
+    lat = float(camera["lat"])
+
+def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    stream_consumer.events.on_data_received = on_event_data_handler
+
+
+# subscribe to new streams being received
+topic_consumer.on_stream_received = on_stream_received_handler
+
+print("Listening to streams. Press CTRL-C to exit.")
+
+# Handle termination signals and provide a graceful exit
+qx.App.run()

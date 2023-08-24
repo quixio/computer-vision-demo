bb0f5716e61be662bba25338487d630c59431776
Steve Rosam <srosam+quix-demos@gmail.com>
2023-08-24 14:24:14 +0000

    Edited 'main.py'

--- Geofence/main.py
+++ Geofence/main.py
@@ -3,23 +3,15 @@ import os
 import pandas as pd
 import json
 import time
+from shapely.geometry import Point, Polygon
+import ast
 
-
+# setup camera coordinates and fence area
 coords = os.environ["fence_coordinates"]
-fence_coords = coords.split(',')
-north = float(fence_coords[0])
-south = float(fence_coords[1])
-east = float(fence_coords[2])
-west = float(fence_coords[3])
+area_of_interest = ast.literal_eval(coords)
+area_of_interest_polygon = Polygon(area_of_interest)
 
-if north < south:
-    print("North<South")
-
-if east < west:
-    print("East<West")
-
-
-
+# create the QuixStreamingClient
 client = qx.QuixStreamingClient()
 
 topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "geofence", 
@@ -29,18 +21,17 @@ topic_producer = client.get_topic_producer(os.environ["output"])
 pd.set_option('display.max_columns', None)
 
 def on_event_data_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
-    print("data")
     camera = json.loads(data.value)
     lon = float(camera["lon"])
     lat = float(camera["lat"])
 
-    print("lt east {}", lat < east)
-    print("lt west {}", lat > west)
-    print("lt north {}", lon > north)
-    print("lt south {}", lon < south)
+    camera_position = Point(lon, lat)
+    in_fence = area_of_interest_polygon.contains(camera_position)
 
-    in_fence = (lon < east) & (lon > west) & (lat > north) & (lat < south)
-    print(in_fence)
+    if in_fence:
+        print(f"Camera is inside the geofence? = {in_fence}")
+        print(data)
+
     camera_id = camera["id"]
 
     if in_fence:

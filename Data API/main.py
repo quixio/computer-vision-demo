524dbeea6e24c28fb934857b1e165156baec94b1
SteveRosam <steve@quix.io>
2023-08-30 15:47:14 +0100

    Add vehicle count endpoint, add images to detected_objects

--- Data API/main.py
+++ Data API/main.py
@@ -1,9 +1,10 @@
 import quixstreams as qx
 import pandas as pd
-from flask import Flask
+from flask import Flask, request, abort
 from flask_cors import CORS
 import os
- 
+import base64
+
 
 pd.set_option('display.max_columns', None)
 
@@ -11,6 +12,8 @@ pd.set_option('display.max_columns', None)
 max_vehicles = {}
 #keep the latest detected objects for each cam
 detected_objects = {}
+# keep the vehicles for each cam
+vehicles = {}
 
 # get a state manager from the Quix client library
 storage = qx.LocalFileStorage()
@@ -26,6 +29,15 @@ if storage.contains_key("detected_objects"):
     detected_objects = storage.get("detected_objects")
     print("detected_objects loaded from state")
 
+# if vehicles is in state, init the property with it
+if storage.contains_key("vehicles"):
+    vehicles = storage.get("vehicles")
+    # sometimes image is seen in this data.
+    # it's not needed so remove it.
+    for key, value in vehicles.items():
+        value.pop('image', None)
+    print("vehicles loaded from state")
+
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
@@ -33,12 +45,16 @@ client = qx.QuixStreamingClient()
 print("Opening input topic")
 max_veh_topic = client.get_topic_consumer(os.environ["input"])
 object_topic = client.get_topic_consumer(os.environ["objects"])
+veh_topic = client.get_topic_consumer(os.environ["vehicles"])
+
 
 def on_max_veh_stream_received_handler(stream_consumer: qx.StreamConsumer):
     global max_vehicles
 
     def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-        print(f'MAX_VEHICLES: stream:{stream_consumer.stream_id}, data={df["max_vehicles"][0]}')
+        print("Receiving max vehicle data")
+
+        #print(f'MAX_VEHICLES: stream:{stream_consumer.stream_id}, data={df["max_vehicles"][0]}')
         max_vehicles[stream_consumer.stream_id] = df["max_vehicles"][0]
         storage.set("max_vehicles", max_vehicles)
     
@@ -46,23 +62,53 @@ def on_max_veh_stream_received_handler(stream_consumer: qx.StreamConsumer):
 
 def on_object_stream_received_handler(stream_consumer: qx.StreamConsumer):
     global detected_objects
+    encoding = 'utf-8'
 
     def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-        df["image"] = '' # we dont need the image for this
-        print(f'OBJECT DETECTED: stream:{stream_consumer.stream_id}, data={df.to_dict()}')
-        detected_objects[stream_consumer.stream_id] = df.to_dict()
+        print("Receiving detected object data")
+
+        base64_bytes = base64.b64encode(df["image"][0])
+        base64_string = base64_bytes.decode(encoding)
+
+        df["image"] = base64_string
+
+        #print(f'OBJECT DETECTED: stream:{stream_consumer.stream_id}, data={df.to_dict()}')
+        detected_objects[stream_consumer.stream_id] = df.to_dict("records")[0]
+
         storage.set("detected_objects", detected_objects)
         
     stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
 
-# init the flas app
+
+def on_vehicles_stream_received_handler(stream_consumer: qx.StreamConsumer):
+    global detected_objects
+
+    def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+        print("Receiving vehicles data")
+        # keep only the timestamp and vehicle count.
+        df = df[['timestamp', 'vehicles']]
+
+        #print(f'VEHICLES: stream:{stream_consumer.stream_id}, data={df.to_dict("records")[0]}')
+        vehicles[stream_consumer.stream_id] = df.to_dict('records')[0]
+        storage.set("vehicles", detected_objects)
+
+    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+
+
+# init the flask app
 app = Flask(__name__)
 CORS(app)
 
 # create the default route
 @app.route("/")
 def index():
-    return "Endpoints are:<br/>detected_objects<br/>max_vehicles"
+    root = request.url_root
+    print(root)
+    return f"Endpoints are:" \
+           f"<br/><a href='{root}detected_objects'>{root}detected_objects</a>" \
+           f"<br/><a href='{root}detected_objects/[camera_id]'>{root}detected_objects/[camera_id]</a>" \
+           f"<br/><a href='{root}max_vehicles'>{root}max_vehicles</a>" \
+           f"<br/><a href='{root}vehicles'>{root}vehicles</a>"
 
 # create the max_vehicles route
 @app.route("/max_vehicles")
@@ -73,6 +119,21 @@ def maximum_vehicles():
 @app.route("/detected_objects")
 def objects():
     return detected_objects
+
+# create the detected objects route for specific camera
+@app.route("/detected_objects/<camera_id>")
+def objects_for_cam(camera_id):
+    print(camera_id)
+    if camera_id in detected_objects:
+        return detected_objects[camera_id]
+    else:
+        abort(404)
+
+# create the vehicles route
+@app.route("/vehicles")
+def cam_vehicles():
+    return vehicles
+
 
 if __name__ == "__main__":
     print("main..")
@@ -88,7 +149,12 @@ if __name__ == "__main__":
     # subscribe to data arriving into the topic
     object_topic.subscribe()
 
-    # you can use app.run for dev, but its not secure, stable or particularly efficient
+    # hook up the stream received handler
+    veh_topic.on_stream_received = on_vehicles_stream_received_handler
+    # subscribe to data arriving into the topic
+    veh_topic.subscribe()
+
+    # you can use app.run for dev, but it's not secure, stable or particularly efficient
     # app.run(debug=True, host="0.0.0.0", port=80)
 
     # use waitress instead for production

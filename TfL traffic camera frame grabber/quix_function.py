de4c5d21047f6da6060fa9e262384ba7e37aade1
Quix Demo <demo@quix.ai>
2023-08-31 09:30:46 +0000

    Edited 'quix_function.py'

--- TfL traffic camera frame grabber/quix_function.py
+++ TfL traffic camera frame grabber/quix_function.py
@@ -30,31 +30,34 @@ class QuixFunction:
         if last_image_state.value is None or timestamp > last_image_state.value:
             print(stream_consumer.stream_id + " updated.")
             last_image_state.value = timestamp
+        else:
+            print("{0} from {1} has been processed. Skipping...".format(camera_video_feed["value"], timestamp))
+            return
 
-            video_stream = cv2.VideoCapture(camera_video_feed["value"])
+        video_stream = cv2.VideoCapture(camera_video_feed["value"])
 
-            count = 0
+        count = 0
+
+        success, image = video_stream.read()
+        while success:
+            frame = cv2.imencode('.jpg', image)
+            if len(frame) <= 1:
+                print("no data")
+                continue
+            
+            frame_bytes = frame[1]
 
             success, image = video_stream.read()
-            while success:
-                frame = cv2.imencode('.jpg', image)
-                if len(frame) <= 1:
-                    print("no data")
-                    continue
-                
-                frame_bytes = frame[1]
+            count += 1
 
-                success, image = video_stream.read()
-                count += 1
-
-                if (count - 1) % self.frame_rate == 0:
-                    self.stream_producer.timeseries.buffer.add_timestamp(timestamp) \
-                        .add_value("image", bytearray(frame_bytes)) \
-                        .add_value("lon", lon) \
-                        .add_value("lat", lat) \
-                        .publish()
-                        
-                    print("Sent {0} frame {1}".format(camera_id, count))
+            if (count - 1) % self.frame_rate == 0:
+                self.stream_producer.timeseries.buffer.add_timestamp(timestamp) \
+                    .add_value("image", bytearray(frame_bytes)) \
+                    .add_value("lon", lon) \
+                    .add_value("lat", lat) \
+                    .publish()
+                    
+                print("Sent {0} frame {1}".format(camera_id, count))
 
 
 

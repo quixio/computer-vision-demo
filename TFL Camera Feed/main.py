950be644a1027e24ab73ca1fb1b3e5439e061210
Quix Demo <demo@quix.ai>
2023-08-31 10:25:48 +0000

    Edited 'main.py'

--- TFL Camera Feed/main.py
+++ TFL Camera Feed/main.py
@@ -34,7 +34,7 @@ def get_data():
         # get root element
         root = tree.getroot()
     
-        print(root["ListBucketResult"])
+        print(root.get("ListBucketResult"))
 
         return
 

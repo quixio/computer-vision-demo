1ea8ec3e7bd51a25aec51ba8b25e9954d2e8fe50
Steve Rosam <srosam+quix-demos@gmail.com>
2023-09-18 14:49:44 +0000

    Edited 'data.service.ts'

--- TfL image processing UI/src/app/services/data.service.ts
+++ TfL image processing UI/src/app/services/data.service.ts
@@ -15,17 +15,41 @@ export class DataService {
 
   getMaxVehicles(): Observable<{ [key: string]: number }> {
     const url = `${this.url}/max_vehicles`
-    return this.httpClient.get<{ [key: string]: number }>(url)
+    return this.httpClient.get(url, { responseType: 'text' }).pipe(
+      map((response) => this.sanitizeData(response))
+    )
   }
 
   getDetectedObjects(id?: string): Observable<{ [key: string]: any }> {
     let url = `${this.url}/detected_objects`
     if (id) url += `/${id}` 
-    return this.httpClient.get<{ [key: string]: number }>(url)
+    return this.httpClient.get(url, { responseType: 'text' }).pipe(
+      map((response) => this.sanitizeData(response))
+    )
   }
 
   getVehicles(): Observable<{ [key: string]: any }> {
     const url = `${this.url}/vehicles`
-    return this.httpClient.get<{ [key: string]: number }>(url)
+    return this.httpClient.get(url, { responseType: 'text' }).pipe(
+      map((response) => this.sanitizeData(response))
+    )
+  }
+
+  /**
+   * Util method used for preprocessing the data and sanitizing it
+   * so that it doesn't contain NaN. It will find and replace that with 0 instead.
+   * This will ensure that it will not fall over when being used on the map to place
+   * markers.
+   * 
+   * @param response - The response from the server as a string 
+   * @returns - A sanitized version of the object
+   */
+  sanitizeData(response: string): { [key: string]: number } {
+    // Preprocess the response text to replace "NaN" variations with "null"
+    const sanitizedResponseText = response.replace(/"NaN"|NaN|"nan"|null|undefined/gi, '0');
+    // Parse the sanitized string response into an object
+    const val: { [key: string]: any } = JSON.parse(sanitizedResponseText);
+    // Return sanitized value
+    return val;
   }
 }

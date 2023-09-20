bb01e58b3b3aeb68810799e3bc3b2ff53d6e5626
Quix Demo <demo@quix.ai>
2023-09-20 13:11:20 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -21,7 +21,7 @@ qx.Logging.update_factory(qx.LogLevel.Debug)
 print("Opening input topic")
 buffered_stream_data = client.get_topic_consumer(
     os.environ["buffered_stream"], 
-    "data-api-v4", 
+    "data-api-v5", 
     auto_offset_reset=qx.AutoOffsetReset.Earliest)
 
 

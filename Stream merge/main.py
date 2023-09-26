21017d83e29c639264193a0e08ec0863668ab5b0
Peter Nagy <peter+demo@quix.io>
2023-09-26 19:58:40 +0000

    Edited 'main.py'

--- Stream merge/main.py
+++ Stream merge/main.py
@@ -2,14 +2,13 @@ import quixstreams as qx
 from quix_function import QuixFunction
 import os
 
+qx.Logging.update_factory(qx.LogLevel.Debug)
 
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
 
 # Change consumer group to a different constant if you want to run model locally.
-print("Opening input and output topics")
-
 print("Opening input and output topics")
 topic_consumer = client.get_topic_consumer(os.environ["input"], "stream-merger",
                                            auto_offset_reset = qx.AutoOffsetReset.Latest)

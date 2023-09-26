21c3375a19636f80e27f83575008b6911508a952
Emanuel x <emanuel+demo@quix.io>
2023-09-26 11:26:58 +0000

    Edited 'main.py'

--- Data Buffer/main.py
+++ Data Buffer/main.py
@@ -21,7 +21,7 @@ def on_image_stream_received_handler(stream_consumer: qx.StreamConsumer):
         df["TAG__camera"] = stream_consumer.stream_id
 
 
-        df["TAG__camera"] = stream_consumer.stream_id
+        df["CameraId"] = stream_consumer.stream_id
         stream_producer.timeseries.buffer.buffer_timeout = buffer_duration
         stream_producer.timeseries.buffer.time_span_in_milliseconds = buffer_duration
         stream_producer.timeseries.buffer.publish(df)

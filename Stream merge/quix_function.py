7943e912718bf6e227518535e496581b8fa92e60
Peter Nagy <peter+demo@quix.io>
2023-09-26 19:56:51 +0000

    Edited 'quix_function.py'

--- Stream merge/quix_function.py
+++ Stream merge/quix_function.py
@@ -19,11 +19,11 @@ class QuixFunction:
     # Callback triggered for each new parameter data.
     def on_dataframe_handler(self, stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
 
-        df["TAG__parent_streamId"] = self.consumer_stream.stream_id
-        df['image'] = df["image"].apply(lambda x: str(base64.b64encode(x).decode('utf-8')))
-
         last_row = df.iloc[-1:]
         print(f"Last time received: {last_row['timestamp']}")
+
+        df["TAG__parent_streamId"] = self.consumer_stream.stream_id
+        df['image'] = df["image"].apply(lambda x: str(base64.b64encode(x).decode('utf-8')))
 
         self.producer_topic.get_or_create_stream("image-feed") \
             .timeseries.publish(df)

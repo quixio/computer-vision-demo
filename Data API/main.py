8aa03ba1c16383eebe17dd3fccb2e72e7e384a60
Emanuel x <emanuel+demo@quix.io>
2023-09-26 15:05:41 +0000

    Edited 'main.py'

--- Data API/main.py
+++ Data API/main.py
@@ -16,6 +16,7 @@ if not os.path.exists("state/camera_images"):
 # Quix injects credentials automatically to the client.
 # Alternatively, you can always pass an SDK token manually as an argument.
 client = qx.QuixStreamingClient()
+qx.App.set_state_storage(qx.InMemoryStorage())
 
 qx.Logging.update_factory(qx.LogLevel.Debug)
 
